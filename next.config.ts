a09c15106b9b06ace13cdb71305b77369ce088ae
lcdamy <zudanga@gmail.com>
2025-08-12 14:12:02 +0200

    feat: add ESLint configuration and update Next.js config formatting

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
-    images: {
+  images: {
     domains: ['cdn-icons-png.flaticon.com', 'rci-files.lon1.digitaloceanspaces.com'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
